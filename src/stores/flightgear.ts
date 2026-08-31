94f9a3ef40416cdb45b08dd435fa7f1fcc9184b8
Philips Nguyen <99601790+philip2012@users.noreply.github.com>
2026-08-31 14:29:57 +0700

    feat: stream live FlightGear telemetry

--- src/stores/flightgear.ts
+++ src/stores/flightgear.ts
@@ -17,6 +17,15 @@ export const useFlightGearStore = defineStore('flightgear', () => {
   const connected = ref(false)
   let socket: WebSocket | null = null
 
+  function subscribeTo(node: string) {
+    socket?.send(
+      JSON.stringify({
+        command: 'addListener',
+        node,
+      }),
+    )
+  }
+
   function connect() {
     if (socket && socket.readyState !== WebSocket.CLOSED) {
       return
@@ -26,18 +35,52 @@ export const useFlightGearStore = defineStore('flightgear', () => {
     socket.onopen = () => {
       connected.value = true
 
-      socket?.send(
-        JSON.stringify({
-          command: 'addListener',
-          node: 'position/altitude-ft',
-        }),
-      )
+      subscribeTo('position/altitude-ft')
+      subscribeTo('velocities/airspeed-kt')
+      subscribeTo('orientation/heading-deg')
+      subscribeTo('velocities/groundspeed-kt')
+      subscribeTo('velocities/vertical-speed-fps')
+      subscribeTo('orientation/pitch-deg')
+      subscribeTo('position/latitude-deg')
+      subscribeTo('position/longitude-deg')
+      subscribeTo('orientation/track-deg')
     }
 
     socket.onmessage = (event) => {
       try {
         const data = JSON.parse(event.data)
-        altitudeFt.value = Number(data.value)
+
+        switch (data.path) {
+          case '/position/altitude-ft':
+            altitudeFt.value = Number(data.value)
+            break
+
+          case '/velocities/airspeed-kt':
+            airspeedKt.value = Number(data.value)
+            break
+
+          case '/orientation/heading-deg':
+            headingDeg.value = Number(data.value)
+            break
+          case '/velocities/groundspeed-kt':
+            groundspeedKt.value = Number(data.value)
+            break
+          case '/velocities/vertical-speed-fps':
+            verticalSpeedFpm.value = Number(data.value)
+            break
+          case '/orientation/pitch-deg':
+            pitchDeg.value = Number(data.value)
+            break
+          case '/position/latitude-deg':
+            latitudeDeg.value = Number(data.value)
+            break
+          case '/position/longitude-deg':
+            longitudeDeg.value = Number(data.value)
+            break
+          case '/orientation/track-deg':
+            trackDeg.value = Number(data.value)
+            break
+        }
       } catch (error) {
         console.error('Failed to parse FlightGear message:', error)
       }
